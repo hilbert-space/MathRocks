c762d81bf0ceed5b1040f10e31e5715f1444fdc8
Ivan Ukhov <uvsoft@gmail.com>
2012-10-09 13:50:35 +0200

    ProbabilityDistribution/display

--- ProbabilityTheory/+ProbabilityDistribution/@Base/Base.m
+++ ProbabilityTheory/+ProbabilityDistribution/@Base/Base.m
@@ -13,6 +13,12 @@ classdef Base < handle
       data = this.sample(options.get('samples', 1e3), 1);
       Stats.observe(data, 'draw', true, options);
     end
+
+    function display(this)
+      fprintf('Probability distribution:\n');
+      fprintf('  Expectation: %.2f\n', this.mu);
+      fprintf('  Variance:    %.2f\n', this.sigma);
+    end
   end
 
   methods (Abstract)
