8c4d761479ff14127d28b4890a6002751c603a17
Ivan Ukhov <uvsoft@gmail.com>
2012-10-29 09:41:59 +0100

    Change a bit the power profile generation

--- SystemSimulation/@PowerProfile/PowerProfile.m
+++ SystemSimulation/@PowerProfile/PowerProfile.m
@@ -25,9 +25,8 @@ classdef PowerProfile < handle
       for i = 1:processorCount
         for j = find(schedule.mapping == i)
           s = 1 + floor(startTime(j) / dt);
-          e = 1 + floor((startTime(j) + executionTime(j)) / dt);
-          e = min([ e, stepCount + 1 ]);
-          profile(i, s:(e - 1)) = processors{i}.dynamicPower(tasks{j}.type);
+          e = min(stepCount, floor((startTime(j) + executionTime(j)) / dt));
+          profile(i, s:e) = processors{i}.dynamicPower(tasks{j}.type);
         end
       end
     end
