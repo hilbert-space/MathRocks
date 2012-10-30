18468df30d8a1f2e7d5fa7b444a0c82c807b06e2
Ivan Ukhov <uvsoft@gmail.com>
2012-10-30 18:23:12 +0100

    Add SystemSimulation/+Test/Assets

--- SystemSimulation/+Test/parseTGFF.m
+++ SystemSimulation/+Test/parseTGFF.m
@@ -1,6 +1,7 @@
 %% Load a platform and an application.
 %
-[ platform, application ] = parseTGFF('002_020.tgff');
+[ platform, application ] = parseTGFF( ...
+  File.join(File.trace, 'Assets', '002_020.tgff'));
 
 display(platform);
 display(application);
