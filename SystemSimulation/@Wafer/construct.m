c98b70b122233250400d1e1a269ff8c16dbf2486
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-01-30 09:15:06 +0100

    Change the layout algorithm for Wafer (gaps between dies)

--- SystemSimulation/@Wafer/construct.m
+++ SystemSimulation/@Wafer/construct.m
@@ -9,35 +9,38 @@ function construct(this, options)
   X = dieFloorplan(:, 3);
   Y = dieFloorplan(:, 4);
 
-  dieW = max(X + W);
-  dieH = max(Y + H);
+  DW = max(X + W);
+  DH = max(Y + H);
+  DS = max(DW, DH);
 
-  waferCenterX = dieW * columns / 2;
-  waferCenterY = dieH * rows / 2;
+  waferCenterX = DS * columns / 2;
+  waferCenterY = DS * rows / 2;
 
-  floorplan = zeros(0, 2);
+  floorplan = zeros(0, 4);
 
   for i = 1:rows
     for j = 1:columns
-      x = (j - 0.5) * dieW;
-      y = (i - 0.5) * dieH;
+      x = (j - 0.5) * DS;
+      y = (i - 0.5) * DS;
       e = ((x - waferCenterX) / waferCenterX)^2 + ...
-        ((y - waferCenterY) / waferCenterY)^2;
+          ((y - waferCenterY) / waferCenterY)^2;
       if e > 1, continue; end
       floorplan(end + 1, :) = [ ...
-        (j - 1) * dieW - waferCenterX, ...
-        (i - 1) * dieH - waferCenterY ];
+        (j - 1) * DS + (DS - DW) / 2 - waferCenterX, ...
+        (i - 1) * DS + (DS - DH) / 2 - waferCenterY, ...
+        (j - 1) * DS                 - waferCenterX, ...
+        (i - 1) * DS                 - waferCenterY ];
     end
   end
 
   this.floorplan = floorplan;
-  this.width  = max(floorplan(:, 1)) + dieW - min(floorplan(:, 1));
-  this.height = max(floorplan(:, 2)) + dieH - min(floorplan(:, 2));
+  this.width  = DS * columns;
+  this.height = DS * rows;
   this.radius = sqrt((this.width / 2)^2 + (this.height / 2)^2);
 
   this.dieFloorplan = dieFloorplan;
-  this.dieWidth = dieW;
-  this.dieHeight = dieH;
+  this.dieWidth = DW;
+  this.dieHeight = DH;
 
   this.dieCount = size(floorplan, 1);
   this.processorCount = size(dieFloorplan, 1);
--- SystemSimulation/@Wafer/plot.m
+++ SystemSimulation/@Wafer/plot.m
@@ -5,36 +5,32 @@ function plot(this, index)
   DF = this.dieFloorplan;
   DW = this.dieWidth;
   DH = this.dieHeight;
+  DS = max(DW, DH);
 
   W = DF(:, 1);
   H = DF(:, 2);
   X = DF(:, 3);
   Y = DF(:, 4);
 
-  figure('Position', [ 250 250 500 500 ]);
+  figure('Position', [ 250, 250, 500, 500 ]);
 
   for i = 1:size(F, 1)
-    x = F(i, 1);
-    y = F(i, 2);
-
-    h = draw(x, y, DW, DH);
-
-    if ismember(i, index)
-      set(h, 'FaceColor', 0.17 * [ 1 1 1 ]);
-      set(h, 'EdgeColor',        [ 1 1 1 ]);
-    else
-      set(h, 'FaceColor', 0.70 * [ 1 1 1 ]);
-      set(h, 'EdgeColor',        [ 1 1 1 ]);
-
-      for j = 1:size(DF, 1)
-        h = draw(x + X(j), y + Y(j), W(j), H(j));
+    h = draw(F(i, 3), F(i, 4), DS, DS);
+    set(h, 'FaceColor', 0.90 * [ 1 1 1 ]);
+    set(h, 'EdgeColor', 0.15 * [ 1 1 1 ]);
+    for j = 1:size(DF, 1)
+      h = draw(F(i, 1) + X(j), F(i, 2) + Y(j), W(j), H(j));
+      if ismember(i, index)
+        set(h, 'FaceColor', 0.25 * [ 1 1 1 ]);
+      else
         set(h, 'FaceColor', Color.pick(j));
-        set(h, 'EdgeColor', [ 1 1 1 ]);
       end
+      set(h, 'EdgeColor', 0.00 * [ 0 0 0 ]);
     end
   end
 
   Plot.title('%d dies, %d cores each', prod(size(F)), length(W));
+  axis tight;
 end
 
 function h = draw(x, y, W, H)
