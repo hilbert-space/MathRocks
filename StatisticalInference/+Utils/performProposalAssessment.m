cd67b2de074d456ac1e6e33a8ce1a640903b8828
Ivan Ukhov <uvsoft@gmail.com>
2013-02-18 16:48:51 +0100

    Parallelize performProposalAssessment

--- StatisticalInference/+Utils/performProposalAssessment.m
+++ StatisticalInference/+Utils/performProposalAssessment.m
@@ -1,5 +1,5 @@
 function assessment = performProposalAssessment( ...
-  computeLogPosterior, theta, covariance, varargin)
+  logPosteriorFunction, theta, covariance, varargin)
 
   options = Options(varargin{:});
   pointCount = options.get('pointCount', 30);
@@ -7,7 +7,7 @@ function assessment = performProposalAssessment( ...
   assert(rem(pointCount, 2) == 0); % Keep it even!
 
   parameterCount = length(theta);
-  logPosteriorMode = computeLogPosterior(theta);
+  logPosteriorMode = feval(logPosteriorFunction, theta);
 
   %
   % Compute approximate conditional standard deviations.
@@ -23,26 +23,30 @@ function assessment = performProposalAssessment( ...
   upperBound = theta + 4 * deviation;
   stepLength = (8 * deviation) / (pointCount - 1);
 
+  Grid = repmat(theta, [ 1, parameterCount, pointCount + 1 ]);
   for i = 1:parameterCount
     grid = lowerBound(i):stepLength(i):upperBound(i);
     grid = [ grid(1:(pointCount / 2)), theta(i), ...
       grid((pointCount / 2 + 1):pointCount) ];
+    Grid(i, i, :) = grid;
+  end
+  Grid = reshape(Grid, parameterCount, []);
 
-    logPosterior = zeros(1, length(grid));
+  logPosterior = zeros(1, size(Grid, 2));
+  parfor i = 1:size(Grid, 2)
+    logPosterior(i) = feval(logPosteriorFunction, Grid(:, i));
+  end
+  logPosterior = reshape(logPosterior, parameterCount, pointCount + 1);
+  Grid = reshape(Grid, parameterCount, parameterCount, pointCount + 1);
 
-    point = theta;
-    for j = 1:length(grid)
-      point(i) = grid(j);
-      logPosterior(j) = computeLogPosterior(point);
-    end
-
+  for i = 1:parameterCount
     %
     % Keep only grid points with the log-posterior not
     % more than 10 units away form the log-posterior mode.
     %
-    I = find((logPosteriorMode - logPosterior) < 10);
-    grid = grid(I);
-    logPosterior = logPosterior(I);
+    I = find((logPosteriorMode - logPosterior(i, :)) < 10);
+
+    grid = squeeze(Grid(i, i, I));
 
     logPosteriorApproximation = ...
       computeLogGaussianDensity(grid, theta(i), deviation(i)) - ...
@@ -50,7 +54,7 @@ function assessment = performProposalAssessment( ...
       logPosteriorMode;
 
     assessment(i).grid = grid;
-    assessment(i).logPosterior = logPosterior;
+    assessment(i).logPosterior = logPosterior(i, I);
     assessment(i).logPosteriorApproximation = logPosteriorApproximation;
   end
 end
