da78c8f767d2cf9572f21628bd4313c5cabe4a71
Ivan Ukhov <uvsoft@gmail.com>
2013-07-04 11:22:03 +0200

    Data.compare in parallel

--- DataAnalysis/+Data/compare.m
+++ DataAnalysis/+Data/compare.m
@@ -113,14 +113,22 @@ function [ globalError, localError ] = compare3D(oneData, twoData, options)
   draw = options.draw;
   options = Options(options, 'draw', false);
 
-  h = Bar('Comparison: step %d out of %d.', codimensionCount);
-
   localError = zeros(dimensionCount, codimensionCount);
 
-  for i = 1:codimensionCount
-    [ ~, localError(:, i) ] = compare2D( ...
-      oneData(:, :, i), twoData(:, :, i), options);
-    increase(h);
+  if matlabpool('size') > 0
+    h = Bar(sprintf('Comparison of %d steps in parallel...', codimensionCount), 100, 50);
+    parfor i = 1:codimensionCount
+      [ ~, localError(:, i) ] = compare2D( ...
+        oneData(:, :, i), twoData(:, :, i), options);
+    end
+    close(h);
+  else
+    h = Bar('Comparison: step %d out of %d.', codimensionCount);
+    for i = 1:codimensionCount
+      [ ~, localError(:, i) ] = compare2D( ...
+        oneData(:, :, i), twoData(:, :, i), options);
+      increase(h);
+    end
   end
 
   globalError = mean(localError(:));
--- Visualization/Bar.m
+++ Visualization/Bar.m
@@ -8,7 +8,9 @@ classdef Bar < handle
 
   methods
     function this = Bar(template, total, done)
+      if nargin < 2, total = 1; end
       if nargin < 3, done = 0; end
+
       this.template = template;
       this.total = total;
       this.done = done;
@@ -21,8 +23,17 @@ classdef Bar < handle
         'edgecolor', color, 'facecolor', color);
     end
 
+    function set(this, done)
+      this.done = done;
+      this.update;
+    end
+
     function increase(this)
       this.done = this.done + 1;
+      this.update;
+    end
+
+    function update(this)
       waitbar(this.done / this.total, this.handle, ...
         sprintf(this.template, this.done, this.total));
       if this.done == this.total, close(this); end
