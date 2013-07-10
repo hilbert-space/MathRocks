1923ef73c843448fea509116dc0101582588569e
Ivan Ukhov <uvsoft@gmail.com>
2013-07-10 16:27:15 +0200

    Two versions of the DSS algorithm

--- SystemSimulation/+Temperature/+Analytical/@DynamicSteadyState/computeWithLeakage.m
+++ SystemSimulation/+Temperature/+Analytical/@DynamicSteadyState/computeWithLeakage.m
@@ -1,9 +1,9 @@
 function [ T, output ] = computeWithLeakage(this, Pdyn, varargin)
   options = Options(varargin{:});
 
-  iterationLimit   = options.get('iterationLimit',   20);
+  iterationLimit   = options.get('iterationLimit', 20);
   temperatureLimit = options.get('temperatureLimit', Utils.toKelvin(1e3));
-  tolerance        = options.get('tolerance',        0.5);
+  tolerance        = options.get('tolerance', 0.5);
 
   nodeCount = this.nodeCount;
   [ processorCount, stepCount ] = size(Pdyn);
@@ -21,73 +21,133 @@ function [ T, output ] = computeWithLeakage(this, Pdyn, varargin)
 
   leakage = this.leakage;
 
-  if ~options.has('L')
-    L = leakage.Lnom * ones(processorCount, 1, stepCount);
-  else
-    assert(size(options.L, 1) == processorCount);
-    L = repmat(options.L, [ 1, 1, stepCount ]);
-  end
+  L = options.get('L', leakage.Lnom * ones(processorCount, 1));
+  assert(size(L, 1) == processorCount);
 
   sampleCount = size(L, 2);
 
-  Pdyn = permute(repmat(Pdyn, [ 1, 1, sampleCount ]), [ 1 3 2 ]);
-
-  T = Tamb * ones(processorCount, sampleCount, stepCount);
-  P = zeros(processorCount, sampleCount, stepCount);
-
-  Q = zeros(nodeCount, sampleCount, stepCount);
-  W = zeros(nodeCount, sampleCount, stepCount);
-  X = zeros(nodeCount, sampleCount, stepCount);
-
-  Tlast = Tamb;
-  I = 1:sampleCount;
   iterationCount = zeros(1, sampleCount);
 
-  for i = 1:iterationLimit
-    P(:, I, :) = Pdyn(:, I, :) + ...
-      leakage.evaluate(L(:, I, :), T(:, I, :));
+  function Version1
+    T = Tamb * ones(processorCount, stepCount, sampleCount);
+    P = zeros(processorCount, stepCount, sampleCount);
 
-    Q(:, I, 1) = D * P(:, I, 1);
-    W(:, I, 1) = Q(:, I, 1);
+    W = zeros(nodeCount, stepCount);
+    X = zeros(nodeCount, stepCount);
 
-    for j = 2:stepCount
-      Q(:, I, j) = D * P(:, I, j);
-      W(:, I, j) = E * W(:, I, j - 1) + Q(:, I, j);
+    for i = 1:sampleCount
+      l = Utils.replicate(L(:, i), 1, stepCount);
+
+      Tlast = Tamb;
+
+      for j = 1:iterationLimit
+        P(:, :, i) = Pdyn + leakage.evaluate(l, T(:, :, i));
+
+        Q = D * P(:, :, i);
+        W(:, 1) = Q(:, 1);
+
+        for k = 2:stepCount
+          W(:, k) = E * W(:, k - 1) + Q(:, k);
+        end
+
+        X(:, 1) = U * diag(1 ./ (1 - exp(dt * ...
+          stepCount * Lambda))) * UT * W(:, stepCount);
+
+        for k = 2:stepCount
+          X(:, k) = E * X(:, k - 1) + Q(:, k - 1);
+        end
+
+        Tcurrent = BT * X + Tamb;
+        T(:, :, i) = Tcurrent;
+
+        if max(max(Tcurrent)) > temperatureLimit
+          %
+          % Thermal runaway
+          %
+          j = Inf;
+          break;
+        end
+
+        if max(max(abs(Tcurrent - Tlast))) < tolerance
+          %
+          % Successful convergence
+          %
+          break;
+        end
+
+        Tlast = Tcurrent;
+      end
+
+      iterationCount(i) = j;
     end
 
-    X(:, I, 1) = U * diag(1 ./ (1 - exp(dt * ...
-      stepCount * Lambda))) * UT * W(:, I, stepCount);
-    T(:, I, 1) = BT * X(:, I, 1) + Tamb;
-
-    for j = 2:stepCount
-      X(:, I, j) = E * X(:, I, j - 1) + Q(:, I, j - 1);
-      T(:, I, j) = BT * X(:, I, j) + Tamb;
-    end
-
-    Tcurrent = T(:, I, :);
-
-    %
-    % Thermal runaway
-    %
-    J = find(max(max(Tcurrent, [], 1), [], 3) > temperatureLimit);
-    iterationCount(I(J)) = Inf;
-
-    %
-    % Successful convergence
-    %
-    K = find(max(max(abs(Tcurrent - Tlast), [], 1), [], 3) < tolerance);
-    iterationCount(I(K)) = i;
-
-    Tlast = Tcurrent;
-
-    M = union(J, K);
-    I(M) = [];
-    Tlast(:, M, :) = [];
-
-    if isempty(I), break; end
+    output.P = P;
   end
 
-  T = permute(T, [ 1, 3, 2 ]);
-  output.P = permute(P, [ 1, 3, 2 ]);
+  function Version2
+    L = repmat(options.L, [ 1, 1, stepCount ]);
+
+    Pdyn = permute(repmat(Pdyn, [ 1, 1, sampleCount ]), [ 1 3 2 ]);
+
+    T = Tamb * ones(processorCount, sampleCount, stepCount);
+    P = zeros(processorCount, sampleCount, stepCount);
+
+    Q = zeros(nodeCount, sampleCount, stepCount);
+    W = zeros(nodeCount, sampleCount, stepCount);
+    X = zeros(nodeCount, sampleCount, stepCount);
+
+    Tlast = Tamb;
+    I = 1:sampleCount;
+
+    for i = 1:iterationLimit
+      P(:, I, :) = Pdyn(:, I, :) + ...
+        leakage.evaluate(L(:, I, :), T(:, I, :));
+
+      Q(:, I, 1) = D * P(:, I, 1);
+      W(:, I, 1) = Q(:, I, 1);
+
+      for j = 2:stepCount
+        Q(:, I, j) = D * P(:, I, j);
+        W(:, I, j) = E * W(:, I, j - 1) + Q(:, I, j);
+      end
+
+      X(:, I, 1) = U * diag(1 ./ (1 - exp(dt * ...
+        stepCount * Lambda))) * UT * W(:, I, stepCount);
+      T(:, I, 1) = BT * X(:, I, 1) + Tamb;
+
+      for j = 2:stepCount
+        X(:, I, j) = E * X(:, I, j - 1) + Q(:, I, j - 1);
+        T(:, I, j) = BT * X(:, I, j) + Tamb;
+      end
+
+      Tcurrent = T(:, I, :);
+
+      %
+      % Thermal runaway
+      %
+      J = find(max(max(Tcurrent, [], 1), [], 3) > temperatureLimit);
+      iterationCount(I(J)) = Inf;
+
+      %
+      % Successful convergence
+      %
+      K = find(max(max(abs(Tcurrent - Tlast), [], 1), [], 3) < tolerance);
+      iterationCount(I(K)) = i;
+
+      M = union(J, K);
+      I(M) = [];
+
+      if isempty(I), break; end
+
+      Tlast = Tcurrent;
+      Tlast(:, M, :) = [];
+    end
+
+    T = permute(T, [ 1, 3, 2 ]);
+    output.P = permute(P, [ 1, 3, 2 ]);
+  end
+
+  eval([ 'Version', num2str(options.get('version', 1)) ]);
+
   output.iterationCount = iterationCount;
 end
