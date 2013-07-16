39ede1de008b52f33be221ad82e21c8df276ed3f
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-07-16 07:47:06 +0200

    Some little changes in LeakagePower/plot

--- SystemSimulation/+Test/+LeakagePower/plot.m
+++ SystemSimulation/+Test/+LeakagePower/plot.m
@@ -1,16 +1,19 @@
 setup;
 
-iterationCount = 1e3;
+iterationCount = 10;
+pointCount = 5000;
 
-leakage = LeakagePower.LinearInterpolation('filename', ...
-  File.join('+Test', 'Assets', 'inverter_45nm_L5_T1000_08.leak'));
+leakage = LeakagePower.PolynomialRegression('filename', ...
+  File.join('+Test', 'Assets', 'inverter_45nm_L5_T1000_08.leak'), ...
+  'LCount', 50, 'TCount', 50, 'TLimit', Utils.toKelvin([ 0, 400 ]), ...
+  'order', [ 3, 2 ]);
 
 display(leakage);
 plot(leakage);
 
 [ L, T ] = meshgrid( ...
-  linspace(leakage.output.Lmin, leakage.output.Lmax, 100), ...
-  linspace(leakage.output.Tmin, leakage.output.Tmax, 100));
+  linspace(leakage.output.Lmin, leakage.output.Lmax, pointCount), ...
+  linspace(leakage.output.Tmin, leakage.output.Tmax, pointCount));
 
 fprintf('Running %d iterations...\n', iterationCount);
 time = tic;
