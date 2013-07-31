d760085fc49bc45b575edc3888f499ff9dd0a235
Ivan Ukhov <uvsoft@gmail.com>
2013-07-31 12:41:23 +0200

    A small improvement in Plot.title

--- Visualization/+Plot/title.m
+++ Visualization/+Plot/title.m
@@ -1,7 +1,12 @@
-function Title(text, varargin)
-  if length(varargin) > 0
-    text = sprintf(text, varargin{:});
+function Title(varargin)
+  if isa(varargin{1}, 'double')
+    h = varargin{1};
+    varargin(1) = [];
+  else
+    h = gcf;
   end
-  title(text, 'FontSize', 16);
-  set(gcf, 'name', text);
+
+  text = sprintf(varargin{:});
+  title(get(h, 'CurrentAxes'), text, 'FontSize', 16);
+  set(h, 'name', text);
 end
