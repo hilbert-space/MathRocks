8613faacccd8d02e742ef503aeeb5a0588f321b9
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-09-23 13:58:01 +0200

    SystemSimulation: renamed Leff into L

--- SystemSimulation/+Test/+LeakagePower/plotCustomRegression.m
+++ SystemSimulation/+Test/+LeakagePower/plotCustomRegression.m
@@ -1,14 +1,14 @@
 function plotCustomRegression(varargin)
   T = sym('T');
-  Leff = sym('Leff');
+  L = sym('L');
 
   C = sym(zeros(1, 4));
   for i = 1:4
     C(i) = sym(sprintf('C%d', i));
   end
 
-  expression.formula = C(1) * T + C(2) * exp(C(3) + C(4) * Leff);
-  expression.parameters = [ T, Leff ];
+  expression.formula = C(1) * T + C(2) * exp(C(3) + C(4) * L);
+  expression.parameters = [ T, L ];
   expression.coefficients = C;
 
   assess('leakageOptions', Options( ...
@@ -16,6 +16,6 @@ function plotCustomRegression(varargin)
     'expression', expression, ...
     'rangeConstraints', Options( ...
       'T', Utils.toKelvin([ 40, 120 ]), ...
-      'Leff', [ -Inf, Inf ])), ...
+      'L', [ -Inf, Inf ])), ...
     varargin{:});
 end
