c241ed62fe12a1cb37ae7a5b8bf0d637c54b6619
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-10-05 23:14:48 +0200

    Approximation: fixed deriveCovariance of HatBasis

--- Approximation/+Test/+HatBasis/private/deriveCovariance.m
+++ Approximation/+Test/+HatBasis/private/deriveCovariance.m
@@ -8,34 +8,33 @@ function result = deriveCovariance(s1, s2)
   l = max(l1, l2);
   r = min(r1, r2);
 
-  if all(isnumeric([ s1.i, s1.j, s2.i, s2.j ])) && l >= r
-    result = sym(0);
-    return;
-  end
+  c1 = min(r, max(l, yij1));
+  c2 = min(r, max(l, yij1));
 
   result1 = ...
-    - int(y - yij1, y, l, yij1) ...
-    + int(y - yij1, y, yij1, r);
-  result2 = ...
-    - int(y - yij2, y, l, yij2) ...
-    + int(y - yij2, y, yij2, r);
+    - int(y - yij1, y, l, c1) ...
+    + int(y - yij1, y, c1, r);
 
-  c1 = min(yij1, yij2);
-  c2 = max(yij1, yij2);
+  c1 = min(r, max(l, yij2));
+  c2 = min(r, max(l, yij2));
+
+  result2 = ...
+    - int(y - yij2, y, l, c1) ...
+    + int(y - yij2, y, c1, r);
+
+  c1 = min(r, max(l, min(yij1, yij2)));
+  c2 = max(l, min(r, max(yij1, yij2)));
 
   result3 = ...
     + int((y - yij1) * (y - yij2), y,  l, c1) ...
     - int((y - yij1) * (y - yij2), y, c1, c2) ...
     + int((y - yij1) * (y - yij2), y, c2,  r);
 
-  expectation1 = deriveExpectation(s1);
-  expectation2 = deriveExpectation(s2);
-
-  result = (r - l) + ...
+  result = (max(r, l) - l) + ...
     - (mi1 - 1) * result1 ...
     - (mi2 - 1) * result2 ...
     + (mi1 - 1) * (mi2 - 1) * result3 ...
-    - expectation1 * expectation2;
+    - deriveExpectation(s1) * deriveExpectation(s2);
 end
 
 function [ mi, yij, l, r ] = configure(s)
