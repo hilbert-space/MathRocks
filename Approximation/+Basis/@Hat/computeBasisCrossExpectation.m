dd9a3b8209dc5dbd97c4bfd258d7e36e23124e84
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-10-09 22:44:47 +0200

    Approximation: speed up computeBasisCrossExpectation

--- Approximation/+Basis/@Hat/computeBasisCrossExpectation.m
+++ Approximation/+Basis/@Hat/computeBasisCrossExpectation.m
@@ -1,4 +1,4 @@
-function result = computeBasisCrossExpectation(this, I1, J1, I2, J2)
+function result = computeBasisCrossExpectation(~, I1, J1, I2, J2)
   [ II, K ] = sort([ I1(:), I2(:) ], 2);
   JJ = [ J1(:), J2(:) ];
   for i = 1:size(K, 1)
@@ -8,82 +8,50 @@ function result = computeBasisCrossExpectation(this, I1, J1, I2, J2)
   [ IJ, ~, K ] = unique([ II, JJ ], 'rows');
   count = size(IJ, 1);
 
-  [ IJ, ~, H ] = unique( ...
-    [ IJ(:, 1), IJ(:, 3); IJ(:, 2), IJ(:, 4) ], 'rows');
-  [ Yij, Mi, ~, L, R ] = this.computeNodes(IJ(:, 1), IJ(:, 2));
-
   result = zeros(count, 1);
-  for k = 1:count
-    n1 = H(k);
-    n2 = H(k + count);
 
-    i1 = IJ(n1, 1);
-    i2 = IJ(n2, 1);
+  %
+  % Second raw moments (see deriveSecondRawMoment).
+  %
+  % int_0^1 (1 - (mi1 - 1) * |y - yij1|)^2 dy
+  %
+  L = find(IJ(:, 1) == IJ(:, 2));
+  result(L) = 2.^(2 - IJ(L, 1)) / 3;
+  result(L(IJ(L, 1) == 1)) = 1;
+  result(L(IJ(L, 1) == 2)) = 1 / 6;
 
-    j1 = IJ(n1, 2);
-    j2 = IJ(n2, 2);
+  L = setdiff(1:count, L);
+  M = L(IJ(L, 1) == 1);
+  %
+  % l = yij2 - 1 / (mi2 - 1)
+  % r = yij2 + 1 / (mi2 - 1)
+  %
+  % (r - l) - (mi2 - 1) * int_l^r |y - y2| dy
+  %
+  result(M) = 2.^(1 - IJ(M, 2));
+  %
+  % l = 0 or 0.5
+  % r = 0.5 or 1
+  %
+  % (r - l) - (mi2 - 1) * int_l^r |y - y2| dy
+  %
+  result(M(IJ(M, 2) == 2)) = 1 / 4;
 
-    if i1 == i2
-      %
-      % Second raw moments (see deriveSecondRawMoment).
-      %
-      % int_0^1 (1 - (mi1 - 1) * |y - yij1|)^2 dy
-      %
-      if i1 == 1
-        result(k) = 1;
-      elseif i1 == 2
-        result(k) = 1 / 6;
-      else
-        result(k) = 2.^(2 - i1) / 3;
-      end
-      continue;
-    end
+  L = setdiff(L, M);
 
-    %
-    % At this point,
-    %
-    assert(i1 < i2 && L(n1) <= L(n2) && R(n2) <= R(n1));
-
-    if i1 == 1
-      if i2 == 2
-        %
-        % l = 0 or 0.5
-        % r = 0.5 or 1
-        %
-        % (r - l) - (mi2 - 1) * int_l^r |y - y2| dy
-        %
-        result(k) = 1 / 4;
-      else
-        %
-        % l = yij2 - 1 / (mi2 - 1)
-        % r = yij2 + 1 / (mi2 - 1)
-        %
-        % (r - l) - (mi2 - 1) * int_l^r |y - y2| dy
-        %
-        result(k) = 2^(1 - i2);
-      end
-      continue;
-    end
-
-    %
-    % At this point,
-    %
-    assert(i2 > 2);
-
-    %
-    % l = yij2 - 1 / (mi2 - 1)
-    % r = yij2 + 1 / (mi2 - 1)
-    %
-    result(k) = ...
-      ... (r - l)
-      + 2^(2 - i2) ...
-      ... (mi1 - 1) * int_l^r |y - yij1| dy
-      - abs((j2 - 1) * 2^(i1 - 2 * i2 + 2) - (j1 - 1) * 2^(2 - i2)) ...
-      ... (mi2 - 1) * int_l^r |y - yij2| dy
-      - 2^(1 - i2) ...
-      ... (mi1 - 1) * (mi2 - 1) int_l^r |y - yij1| * |y - yij2| dy
-      + abs((j2 - 1) * 2^(i1 - 2 * i2 + 1) - (j1 - 1) * 2^(1 - i2));
-  end
+  %
+  % l = yij2 - 1 / (mi2 - 1)
+  % r = yij2 + 1 / (mi2 - 1)
+  %
+  result(L) = ...
+    ... (r - l)
+    + 2.^(2 - IJ(L, 2)) ...
+    ... (mi1 - 1) * int_l^r |y - yij1| dy
+    - abs((IJ(L, 4) - 1) .* 2.^(IJ(L, 1) - 2 * IJ(L, 2) + 2) - (IJ(L, 3) - 1) .* 2.^(2 - IJ(L, 2))) ...
+    ... (mi2 - 1) * int_l^r |y - yiIJ(L, 4)| dy
+    - 2.^(1 - IJ(L, 2)) ...
+    ... (mi1 - 1) * (mi2 - 1) int_l^r |y - yij1| * |y - yiIJ(L, 4)| dy
+    + abs((IJ(L, 4) - 1) .* 2.^(IJ(L, 1) - 2 * IJ(L, 2) + 1) - (IJ(L, 3) - 1) .* 2.^(1 - IJ(L, 2)));
 
   result = prod(reshape(result(K), size(I1)), 2);
 end
