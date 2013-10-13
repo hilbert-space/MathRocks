c43913c31ed63a3ae2609314d9a960c79f7995d8
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-10-13 11:23:26 +0200

    UQ: added Test.Temperature.sweep

--- UncertaintyQuantification/+Test/+Temperature/private/compare.m
+++ UncertaintyQuantification/+Test/+Temperature/private/compare.m
@@ -49,101 +49,20 @@ function compare(options, secondOptions)
   %
   % Comparison of expectations, variances, and PDFs
   %
-  if Console.question('Compare expectations, variances, and PDFs? ')
-    Plot.temperatureVariation({ oneTexp, twoTexp }, ...
-       { oneOutput.Tvar, twoOutput.Tvar }, ...
-      'time', options.timeLine, 'names', { oneMethod, twoMethod });
+  Plot.temperatureVariation({ oneTexp, twoTexp }, ...
+     { oneOutput.Tvar, twoOutput.Tvar }, ...
+    'time', options.timeLine, 'names', { oneMethod, twoMethod });
 
-    Statistic.compare(Utils.toCelsius(oneOutput.Tdata(:, :, k)), ...
-      Utils.toCelsius(twoOutput.Tdata(:, :, k)), ...
-      'method', 'smooth', 'range', 'unbounded', ...
-      'layout', 'one', 'draw', true, ...
-      'names', { oneMethod, twoMethod });
+  Statistic.compare(Utils.toCelsius(oneOutput.Tdata(:, :, k)), ...
+    Utils.toCelsius(twoOutput.Tdata(:, :, k)), ...
+    'method', 'smooth', 'range', 'unbounded', ...
+    'layout', 'one', 'draw', true, ...
+    'names', { oneMethod, twoMethod });
 
-    Statistic.compare( ...
-      Utils.toCelsius(oneOutput.Tdata), ...
-      Utils.toCelsius(twoOutput.Tdata), ...
-      'method', 'histogram', 'range', 'unbounded', ...
-      'layout', 'separate', 'draw', true, ...
-      'names', { oneMethod, twoMethod });
-  end
-
-  if one.process.dimensions ~= two.process.dimensions, return; end
-
-  parameters = options.processOptions.parameters;
-  dimensions = one.process.dimensions;
-  parameterCount = length(dimensions);
-  names = fieldnames(parameters);
-
-  nominals = cell(1, parameterCount);
-  sweeps = cell(1, parameterCount);
-  for i = 1:parameterCount
-    switch parameters.get(i).model
-    case 'Gaussian'
-      sweeps{i} = -7:0.2:7;
-    otherwise
-      assert(false);
-    end
-    nominals{i} = zeros(length(sweeps{i}), dimensions{i});
-  end
-
-  %
-  % Sweeping the random variables
-  %
-  Iparam = 1;
-  Irv = uint8(1);
-  while Console.question('Sweep random variables? ')
-    if length(names) > 1
-      name = Console.request( ...
-       'prompt', sprintf('Which parameter? [%s] ', names{Iparam}), ...
-       'type', 'char', 'default', names{Iparam});
-
-      found = false;
-      for i = 1:length(names)
-        if strcmp(names{i}, name)
-          found = true;
-          break;
-        end
-      end
-
-      if ~found
-        Iparam = 1;
-        continue;
-      end
-
-      Iparam = i;
-    end
-
-    Irv = Console.request( ...
-     'prompt', sprintf('Which random variable? [%s] ', ...
-       String(Irv)), 'type', 'uint8', 'default', Irv);
-
-    dimensionCount = dimensions(Iparam);
-
-    if any(Irv > dimensionCount)
-      Irv = uint8(1);
-      continue;
-    end
-
-    parameters = nominals;
-    for i = Irv
-      parameters{Iparam}(:, i) = sweeps{Iparam};
-    end
-
-    oneTdata = one.evaluate(oneOutput, parameters);
-    twoTdata = two.evaluate(twoOutput, parameters);
-
-    oneTdata = Utils.toCelsius(oneTdata(:, :, k));
-    twoTdata = Utils.toCelsius(twoTdata(:, :, k));
-
-    figure;
-    for i = 1:options.processorCount
-      color = Color.pick(i);
-      line(rvs, oneTdata(:, i), 'Color', color, 'Marker', 'o');
-      line(rvs, twoTdata(:, i), 'Color', color, 'Marker', 'x');
-    end
-
-    Plot.title('Sweep at %.3f s', timeSlice);
-    Plot.label('Variable', 'Temperature, C');
-  end
+  Statistic.compare( ...
+    Utils.toCelsius(oneOutput.Tdata), ...
+    Utils.toCelsius(twoOutput.Tdata), ...
+    'method', 'histogram', 'range', 'unbounded', ...
+    'layout', 'separate', 'draw', true, ...
+    'names', { oneMethod, twoMethod });
 end
---
+++ UncertaintyQuantification/+Test/+Temperature/sweep.m
@@ -0,0 +1,132 @@
+function sweep(varargin)
+  setup;
+
+  options = Options(varargin{:});
+
+  analysis = options.fetch('analysis', 'Transient');
+  fprintf('Analysis: %s\n', analysis);
+
+  options = Configure.systemSimulation(options);
+  options = Configure.processVariation(options);
+  options = Configure.surrogate(options);
+
+  temperature = Temperature.Analytical.(analysis)(options);
+  process = ProcessVariation(options.processOptions);
+
+  function Tdata = evaluate(parameters)
+    parameters = process.evaluate(parameters);
+    parameters = cellfun(@transpose, parameters, 'UniformOutput', false);
+    parameters = process.assign(parameters);
+    Tdata = permute(temperature.computeWithLeakage( ...
+      options.dynamicPower, parameters), [ 3 1 2 ]);
+  end
+
+  parameters = options.processOptions.parameters;
+  dimensions = process.dimensions;
+  parameterCount = length(dimensions);
+  names = fieldnames(parameters);
+
+  sweeps = cell(1, parameterCount);
+  nominals = cell(1, parameterCount);
+  for i = 1:parameterCount
+    parameter = parameters.(names{i});
+    switch parameter.model
+    case 'Gaussian'
+      sweeps{i} = -4:0.1:4;
+      nominals{i} = zeros(length(sweeps{i}), dimensions(i));
+    case 'Beta'
+      sweeps{i} = -1:0.05:1;
+      nominals{i} = zeros(length(sweeps{i}), dimensions(i));
+    otherwise
+      assert(false);
+    end
+  end
+
+  Iparameter = 1;
+  Ivariable = 1;
+  Istep = round(options.stepCount / 2);
+
+  while true
+    Iparameter = askParameter(names, Iparameter);
+    if isempty(Iparameter)
+      Iparameter = 1;
+      continue;
+    end
+
+    Ivariable = askVariable(dimensions(Iparameter), Ivariable);
+    if isempty(Ivariable)
+      Ivariable = 1;
+      continue;
+    end
+
+    Istep = askStep(options.stepCount, options.samplingInterval, Istep);
+    if isempty(Istep)
+      Istep = round(options.stepCount / 2);
+      continue;
+    end
+
+    parameters = nominals;
+    for i = Ivariable
+      parameters{Iparameter}(:, i) = sweeps{Iparameter};
+    end
+
+    Tdata = evaluate(parameters);
+    Tdata = Utils.toCelsius(Tdata(:, :, Istep));
+
+    Plot.figure(800, 400);
+    for i = 1:options.processorCount
+      Plot.line(sweeps{Iparameter}, Tdata(:, i), 'number', i);
+    end
+
+    Plot.title('Sweep at %.3f s', Istep * options.samplingInterval);
+    Plot.label(sprintf('%s(%s)', names{Iparameter}, String(Ivariable)), ...
+      'Temperature, C');
+
+    if ~Console.question('Sweep more? '), break; end
+  end
+end
+
+function index = askParameter(names, index)
+  if length(names) == 1
+    index = 1;
+    return;
+  end
+
+  name = Console.request( ...
+    'prompt', sprintf('Which parameter (%s)? [%s] ', ...
+    String.join(', ', names), names{index}), ...
+    'type', 'char', 'default', names{index});
+
+  index = [];
+
+  for i = 1:length(names)
+    if strcmpi(names{i}, name)
+      index = i;
+      return;
+    end
+  end
+end
+
+function index = askVariable(dimensionCount, index)
+  if dimensionCount == 1
+    index = 1;
+    return;
+  end
+
+  index = Console.request( ...
+    'prompt', sprintf('Which random variable (up to %d)? [%s] ', ...
+    dimensionCount, String(index)), 'default', index);
+
+  if any(index > dimensionCount), index = []; end
+end
+
+function index = askStep(stepCount, samplingInterval, index)
+  time = Console.request( ...
+    'prompt', sprintf('What moment of time (up to %.2f s)? [%s] ', ...
+    stepCount * samplingInterval, String(index * samplingInterval)), ...
+    'default', index * samplingInterval);
+
+  index = floor(time / samplingInterval);
+
+  if index < 1 || index > stepCount, index = []; end
+end
