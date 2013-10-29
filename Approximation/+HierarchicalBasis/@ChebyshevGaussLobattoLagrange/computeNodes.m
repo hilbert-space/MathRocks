6085813ad3e4c9bcb89fee2f21d1d0b5da00f630
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-10-29 08:15:30 +0100

    Approximation: removed some unnecessary stuff from ChebyshevGaussLobattoLagrange

--- Approximation/+HierarchicalBasis/@ChebyshevGaussLobattoLagrange/computeNodes.m
+++ Approximation/+HierarchicalBasis/@ChebyshevGaussLobattoLagrange/computeNodes.m
@@ -1,27 +1,22 @@
-function [ Yij, offsets, counts, Ji, Mi ] = computeNodes(this, I)
-  [ indexCount, dimensionCount ] = size(I);
+function [ nodes, offsets, counts ] = computeNodes(this, indexes)
+  [ indexCount, dimensionCount ] = size(indexes);
 
-  counts = prod(reshape(this.Ni(I), size(I)), 2);
+  counts = prod(reshape(this.Ni(indexes), size(indexes)), 2);
   offsets = cumsum([ 0; counts(1:(end - 1)) ]);
 
-  Yij = 0.5 * ones(sum(counts), dimensionCount);
-  Ji = ones(size(Yij), 'uint32');
+  nodes = 0.5 * ones(sum(counts), dimensionCount);
 
   for i = 1:indexCount
-    J = I(i, :);
+    J = indexes(i, :);
     K = find(J > 1);
     switch numel(K)
     case 0
     case 1
       range = (offsets(i) + 1):(offsets(i) + counts(i));
-      Yij(range, K) = this.Yij{J(K)};
-      Ji(range, K) = this.Ji{J(K)};
+      nodes(range, K) = this.Yij{J(K)};
     otherwise
       range = (offsets(i) + 1):(offsets(i) + counts(i));
-      Yij(range, K) = Utils.tensor(this.Yij(J(K)));
-      Ji(range, K) = Utils.tensor(this.Ji(J(K)));
+      nodes(range, K) = Utils.tensor(this.Yij(J(K)));
     end
   end
-
-  Mi = reshape(this.Mi(I), size(I));
 end
