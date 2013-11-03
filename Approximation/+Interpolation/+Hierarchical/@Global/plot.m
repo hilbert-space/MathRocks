29dbe8add205416a92133bce9c9b72af2ee40b48
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-11-03 16:47:12 +0100

    Approximation: improved the plotting of sparse grids and their indexes

--- Approximation/+Interpolation/+Hierarchical/@Global/plot.m
+++ Approximation/+Interpolation/+Hierarchical/@Global/plot.m
@@ -1,5 +1,13 @@
 function plot(this, output)
-  nodes = this.basis.computeNodes(output.indexes);
+  [ nodes, offsets, counts ] = this.basis.computeNodes(output.indexes);
+
+  mapping = zeros(size(nodes, 1), 1);
+
+  for i = 1:size(offsets, 1)
+    mapping((offsets(i) + 1):(offsets(i) + counts(i))) = i;
+  end
+
   plot@Interpolation.Hierarchical.Base(this, ...
-    'nodes', nodes, 'indexes', output.indexes);
+    'nodes', nodes, 'nodeMapping', mapping, ...
+    'indexes', output.indexes, 'indexMapping', 1:size(offsets, 1));
 end
