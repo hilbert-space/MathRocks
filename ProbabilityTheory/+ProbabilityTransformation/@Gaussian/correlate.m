502778ed2ce38672b1ad5a6de633afbbfc23560d
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-11-04 11:08:49 +0100

    ProbabilityTheory: added a note in ProbabilityTransformation/@Gaussian/correlate

--- ProbabilityTheory/+ProbabilityTransformation/@Gaussian/correlate.m
+++ ProbabilityTheory/+ProbabilityTransformation/@Gaussian/correlate.m
@@ -8,7 +8,7 @@ function correlation = correlate(this, variables, options)
   nodes = quadrature.nodes;
   weights = quadrature.weights;
 
-  distribution = this.gaussianDistribution;
+  gaussian = this.gaussianDistribution;
 
   optimizationOptions = options.get( ...
     'optimizationOptions', optimset('TolX', 1e-6));
@@ -27,9 +27,20 @@ function correlation = correlate(this, variables, options)
       sigma1 = sqrt(rv1.variance);
       sigma2 = sqrt(rv2.variance);
 
-      weightsOne = weights .* (rv1.icdf(distribution.cdf(nodes(:, 1))) - mu1);
-      two = @(rho) rv2.icdf(distribution.cdf(rho * nodes(:, 1) + sqrt(1 - rho^2) * nodes(:, 2))) - mu2;
-      goal = @(rho) abs(rho0 - sum(weightsOne .* two(rho)) / sigma1 / sigma2);
+      %
+      % NOTE: The Gaussian quadrature cannot be directly applied to compute
+      % the double integral under consideration since the joint density
+      % does not have a product structure (the two variables are dependent).
+      % Therefore, we need to have a little linear transformation inside the
+      % integrand to make the integration be with respect to independent
+      % Gaussian random variables. Another way around, we need to correlate
+      % the nodes of the quadrature.
+      %
+      oneWeights = weights .* (rv1.icdf(gaussian.cdf(nodes(:, 1))) - mu1);
+      two = @(rho) rv2.icdf(gaussian.cdf( ...
+        rho * nodes(:, 1) + sqrt(1 - rho^2) * nodes(:, 2))) - mu2;
+
+      goal = @(rho) abs(rho0 - sum(oneWeights .* two(rho)) / sigma1 / sigma2);
 
       correlation(i, j) = fminbnd(goal, -1, 1, optimizationOptions);
       correlation(j, i) = correlation(i, j);
