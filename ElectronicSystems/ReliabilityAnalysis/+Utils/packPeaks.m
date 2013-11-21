8376326ffbd473d0915976d82fc01763fd29b16b
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-11-21 21:13:40 +0100

    ES/RA: tuned a bit Utils/(un)packPeaks

--- ElectronicSystems/ReliabilityAnalysis/+Utils/packPeaks.m
+++ ElectronicSystems/ReliabilityAnalysis/+Utils/packPeaks.m
@@ -1,8 +1,18 @@
 function P = packPeaks(T, output)
   profileCount = size(T, 3);
+
   T = permute(T, [ 3, 2, 1 ]);
-  P = zeros(profileCount, 0);
+
+  peakCounts = zeros(1, output.processorCount);
   for i = 1:output.processorCount
-    P = [ P, T(:, output.peakIndex{i}, i) ];
+    peakCounts(i) = length(output.peakIndex{i});
+  end
+
+  P = zeros(profileCount, sum(peakCounts));
+
+  offset = 0;
+  for i = 1:output.processorCount
+    P(:, (offset + 1):(offset + peakCounts(i))) = T(:, output.peakIndex{i}, i);
+    offset = offset + peakCounts(i);
   end
 end
--- ElectronicSystems/ReliabilityAnalysis/+Utils/unpackPeaks.m
+++ ElectronicSystems/ReliabilityAnalysis/+Utils/unpackPeaks.m
@@ -1,10 +1,12 @@
 function T = unpackPeaks(P, output)
   profileCount = size(P, 1);
+
   T = zeros(output.processorCount, output.stepCount, profileCount);
-  j = 0;
+
+  offset = 0;
   for i = 1:output.processorCount
-    k = length(output.peakIndex{i});
-    T(i, output.peakIndex{i}, :) = P(:, (j + 1):(j + k)).';
-    j = j + k;
+    count = length(output.peakIndex{i});
+    T(i, output.peakIndex{i}, :) = P(:, (offset + 1):(offset + count)).';
+    offset = offset + count;
   end
 end
