8cdbd5eedd990534e42366aa608301b410108ecd
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-12-03 08:33:04 +0100

    Math/PT: Improved the test for Utils.gaussianToBeta

--- Mathematics/ProbabilityTheory/+Test/+Utils/gaussianToBeta.m
+++ Mathematics/ProbabilityTheory/+Test/+Utils/gaussianToBeta.m
@@ -1,27 +1,72 @@
 function gaussianToBeta
-  close all;
   setup;
 
+  mu = 22.5e-9;
+  sigma = 0.04 * mu;
+  range = mu + [ -3, 3 ] * sigma;
+  contribution = 0.5;
+
   gaussian = ProbabilityDistribution.Gaussian( ...
-    'mu', 0.5 * 50e-9, ...
-    'sigma', sqrt(0.5) * 0.04 * 22.5e-9);
+    'mu', mu, 'sigma', sigma);
 
-  beta = Utils.gaussianToBeta(gaussian, ...
+  gaussian1 = ProbabilityDistribution.Gaussian( ...
+    'mu', contribution * mu, 'sigma', sqrt(contribution) * sigma);
+  gaussian2 = ProbabilityDistribution.Gaussian( ...
+    'mu', (1 - contribution) * mu, 'sigma', sqrt(1 - contribution) * sigma);
+
+  beta1 = Utils.gaussianToBeta(gaussian1, ...
     'target', 'variance', ...
-    'a', 0.5 * 50e-9 - 3 * sqrt(0.5) * 0.04 * 22.5e-9, ...
-    'b', 0.5 * 50e-9 + 3 * sqrt(0.5) * 0.04 * 22.5e-9);
+    'a', contribution * range(1), ...
+    'b', contribution * range(2));
+  beta2 = Utils.gaussianToBeta(gaussian2, ...
+    'target', 'variance', ...
+    'a', (1 - contribution) * range(1), ...
+    'b', (1 - contribution) * range(2));
 
-  x = linspace(beta.a, beta.b, 50);
-  f1 = gaussian.pdf(x);
-  f2 = beta.pdf(x);
+  Plot.figure(1000, 400);
 
-  Plot.figure;
-  Plot.line(x, f1, 'number', 1, 'markEach', 2);
-  Plot.line(x, f2, 'number', 2, 'markEach', 2);
+  subplot(1, 2, 1);
+  x = linspace(beta1.a, beta1.b, 50);
+  Plot.line(x, gaussian1.pdf(x), 'number', 1, 'markEach', 2);
+  Plot.line(x, beta1.pdf(x), 'number', 2, 'markEach', 2);
+  Plot.title('Gaussian1 vs. Beta1');
+  Plot.legend('Gaussian1', 'Beta1');
 
-  Plot.legend( ...
-    sprintf('Gaussian (%s, %s^2)', num2str(gaussian.mu), ...
-      num2str(gaussian.sigma)), ...
-    sprintf('Beta (%s, %s, %s, %s)', num2str(beta.alpha), ...
-      num2str(beta.beta), num2str(beta.a), num2str(beta.b)));
+  subplot(1, 2, 2);
+  x = linspace(beta2.a, beta2.b, 50);
+  Plot.line(x, gaussian2.pdf(x), 'number', 1, 'markEach', 2);
+  Plot.line(x, beta2.pdf(x), 'number', 2, 'markEach', 2);
+  Plot.title('Gaussian2 vs. Beta2');
+  Plot.legend('Gaussian2', 'Beta2');
+
+  Plot.figure(800, 400);
+  x = linspace(beta1.a + beta2.a, beta1.b + beta2.b, 50);
+  Plot.line(x, gaussian.pdf(x), 'number', 1, 'markEach', 2);
+  Plot.line(x, convolute(x, beta1, beta2), 'number', 2, 'markEach', 2);
+  Plot.vline(range(1), 'number', 3);
+  Plot.vline(range(2), 'number', 3);
+  Plot.title('Two Gaussians vs. Two Betas');
+  Plot.legend('Gaussian1 + Gaussian2', 'Beta1 + Beta2');
+
+  fprintf('Variance error: %e\n', ...
+    abs(sigma^2 - beta1.variance - beta2.variance));
+end
+
+function fz = convolute(z, X, Y)
+  fz = zeros(size(z));
+
+  Sx = X.support;
+  Sy = Y.support;
+
+  for i = 1:numel(z)
+    S = [ z(i) - Sy(2), z(i) - Sy(1) ];
+    S = [ max(Sx(1), S(1)), min(Sx(2), S(2)) ];
+
+    if S(1) >= S(2), continue; end
+    S = [ z(i) - S(2), z(i) - S(1) ];
+    S = [ max(Sy(1), S(1)), min(Sy(2), S(2)) ];
+    if S(1) >= S(2), continue; end
+
+    fz(i) = integral(@(y) X.pdf(z(i) - y) .* Y.pdf(y), S(1), S(2));
+  end
 end
