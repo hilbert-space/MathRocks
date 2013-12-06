7fa332b2d57293e18920d70ea98b7e93cef3d8ae
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-12-06 09:26:57 +0100

    String: hungarianize

--- +String/capitalize.m
+++ +String/capitalize.m
@@ -1,4 +1,4 @@
 function string = capitalize(string)
-  string = regexprep(string, '([A-Z])',' ${lower($1)}');
+  string = regexprep(string, '([A-Z])', ' ${lower($1)}');
   string = regexprep(string, '^\s*([a-z])', '${upper($1)}');
 end
