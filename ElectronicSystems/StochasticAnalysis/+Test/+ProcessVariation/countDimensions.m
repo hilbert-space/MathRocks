59d28c8b806d7392129f7c4688dd19b4061b8758
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-12-08 08:53:29 +0100

    ES/SA: added a new test, countDimensions

---
+++ ElectronicSystems/StochasticAnalysis/+Test/+ProcessVariation/countDimensions.m
@@ -0,0 +1,15 @@
+function countDimensions(varargin)
+  setup;
+
+  processorCounts  = [ 2 4 8 16 32 ];
+
+  fprintf('%15s%15s\n', 'Processors', 'Variables');
+  for i = 1:length(processorCounts)
+    options = Configure.systemSimulation( ...
+      varargin{:}, 'processorCount', processorCounts(i));
+    options = Configure.deterministicAnalysis(options);
+    options = Configure.stochasticAnalysis(options);
+    process = ProcessVariation(options.processOptions);
+    fprintf('%15d%15d\n', processorCounts(i), sum(process.dimensions));
+  end
+end
