46d3a14b9efc93790be468e83f30dc7e03da09e7
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-13 13:04:46 +0200

    Added the MonteCarlo package

---
+++ setup.m
@@ -0,0 +1,11 @@
+function setup
+  use('Approximation');
+  use('DataAnalysis');
+  use('FunctionalAnalysis');
+  use('ProbabilityTheory');
+  use('Statistics');
+
+  use('Vendor', 'DataHash');
+  use('Vendor', 'chebfun');
+  use('Vendor', 'heatmaps');
+end
