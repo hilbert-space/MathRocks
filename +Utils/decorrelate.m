378ef811a5aea1d94da2743c7d40fb845e8a77e5
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-13 11:43:00 +0200

    Added the Utils package

---
+++ +Utils/decorrelate.m
@@ -0,0 +1,13 @@
+function [M, I] = decorrelate(V, threshold)
+  [U, L] = pcacov(V); % sorted in the descending order
+
+  M = U * diag(sqrt(L));
+  I = L / sum(L); % importance
+
+  if nargin < 2 || threshold >= 1, return; end
+
+  dimensionCount = Utils.countSignificant(L, threshold);
+
+  M = M(:, 1:dimensionCount);
+  I = I(1:dimensionCount);
+end
