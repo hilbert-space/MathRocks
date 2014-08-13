7c37c981665b4b24a6f62d04d131177ace5caf0d
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-13 11:08:45 +0200

    Added the Utils package

---
+++ +Utils/tensor.m
@@ -0,0 +1,7 @@
+function nodesND = tensor(nodes1D)
+  dimensionCount = length(nodes1D);
+  grid = cell(1, dimensionCount);
+  [grid{:}] = ndgrid(nodes1D{:});
+  nodesND = zeros(numel(grid{1}), dimensionCount, class(grid{1}));
+  for i = 1:dimensionCount, nodesND(:, i) = grid{i}(:); end
+end
