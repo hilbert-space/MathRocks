188ca90c3c0ff59fe1c977f18698636c54225c6a
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-14 12:47:51 +0200

    DA: Fixed Makefile and setup.m

--- DeterministicAnalysis/setup.m
+++ DeterministicAnalysis/setup.m
@@ -1,8 +1,9 @@
 function setup
+  use('Approximation');
   use('DataAnalysis');
   use('DataStructure');
-  use('Mathematics', 'Approximation');
-  use('Mathematics', 'Statistics');
+  use('Statistics');
+
   use('ElectronicSystems', 'CircuitSimulation');
 
   use('Vendor', 'DataHash');
